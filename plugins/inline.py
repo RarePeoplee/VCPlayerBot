c914199d1e9cb24163d1d75f95a95ae07468075e
RarePeoplee <86560013+RarePeoplee@users.noreply.github.com>
2021-09-16 02:10:42 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -22,8 +22,8 @@ from logger import LOGGER
 
 buttons = [
     [
-        InlineKeyboardButton('⚡️Make Own Bot', url='https://github.com/subinps/VCPlayerBot'),
-        InlineKeyboardButton('🧩 Join Here', url='https://t.me/subin_works'),
+        InlineKeyboardButton('⚡️Make Own Bot', url='https://t.me/rarePEOPLE'),
+        InlineKeyboardButton('🧩 Join Here', url='https://t.me/bad_vibe_sucks'),
     ]
     ]
 def get_cmd(dur):
@@ -38,7 +38,7 @@ async def search(client, query):
         answers.append(
             InlineQueryResultArticle(
                 title="Deploy",
-                input_message_content=InputTextMessageContent(f"{Config.REPLY_MESSAGE}\n\n<b>You can't use this bot in your group, for that you have to make your own bot from the [SOURCE CODE](https://github.com/subinps/VCPlayerBot) below.</b>", disable_web_page_preview=True),
+                input_message_content=InputTextMessageContent(f"{Config.REPLY_MESSAGE}\n\n<b>You can't use this bot in your group, for that you have to make your own bot from the [OWNER](https://t.me/rarePEOPLE),
                 reply_markup=InlineKeyboardMarkup(buttons)
                 )
             )
